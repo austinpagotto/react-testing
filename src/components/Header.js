c0c1dcf8b34915bd54c113e4b9f29bb3d3ca5ad2
Austin Pagotto <austinpagotto@gmail.com>
2019-11-17 14:06:38 -0500

    test

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+function Header(){
+    return(
+        <nav>
+            <ul>
+                <li>item 1</li>
+                <li>item 2</li>
+                <li>item 3</li>
+            </ul>
+        </nav>
+    )
+}
+export default Header
---
+++ src/components/MainContent.js
@@ -0,0 +1,9 @@
+import React from 'react';
+function MainContent(){
+    return(
+        <p>
+            main content of the page im the best
+        </p>
+    )
+}
+export default MainContent
