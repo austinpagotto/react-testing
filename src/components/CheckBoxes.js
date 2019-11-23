3a80c9e0d30072f1960250deefd5ab3dbbd96b48
Austin Pagotto <austinpagotto@gmail.com>
2019-11-23 13:49:02 -0500

    todo styles

--- src/components/CheckBoxes.js
+++ src/components/CheckBoxes.js
@@ -1,12 +1,16 @@
 import React from 'react';
 function CheckBoxes(){
+
+    const styles = {
+        fontSize:24,
+    }
     return(
         <div>
-              <input type="checkbox"  name='dishes'/> <label for='dishes'>wash dishes</label>
+              <input type="checkbox"  name='dishes'/> <label for='dishes' style = {styles}>wash dishes</label>
               <br></br>
-              <input type="checkbox"  name='dog'/> <label for='dog'>walk dog</label>
+              <input type="checkbox"  name='dog'/> <label for='dog' style = {styles}>walk dog</label>
               <br></br>
-              <input type="checkbox"  name='practice'/> <label for='practice'>practice react</label>
+              <input type="checkbox"  name='practice'/> <label for='practice' style = {styles}>practice react</label>
         </div>
     )
 }
